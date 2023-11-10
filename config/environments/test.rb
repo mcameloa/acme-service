ed0fc47d84f6664ba33d3eee20d54f3459a63fce
Milton Javier Camelo Arango <javicamelo@outlook.com>
2023-11-09 19:43:27 -0500

    Create ruby.yml
    Add workflow

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,4 +63,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.credentials.devise_jwt_secret_key = ENV['DEVISE_JWT_SECRET_KEY']
 end
